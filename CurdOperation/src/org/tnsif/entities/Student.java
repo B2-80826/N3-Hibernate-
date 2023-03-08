266e1b1db28e250d49bccfe14d6c4e78e07a9db1
vrushsen nanasaheb bansode <74109623+vrush-213@users.noreply.github.com>
2023-03-08 14:01:19 +0530

    Update Student.java

--- CurdOperation/src/org/tnsif/entities/Student.java
+++ CurdOperation/src/org/tnsif/entities/Student.java
@@ -8,11 +8,11 @@ import javax.persistence.Table;
 @Table(name="student")
 
 public class Student {
-	
+	//data
 	@Id
 	private int rollnumber;
 	private String stuentname;
-//getter and setter
+//getter and setter methods 
 	
 public int getRollnumber() {
 		return rollnumber;
